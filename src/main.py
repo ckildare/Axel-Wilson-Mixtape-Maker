3ff6e07a2fa866365883eddf55d6348436550b08
elijahquentin <eli.q.smith@gmail.com>
2022-01-22 16:57:18 -0600

    Added basic search functionality for spotify query.

--- src/main.py
+++ src/main.py
@@ -5,16 +5,23 @@ from spotipy.oauth2 import SpotifyClientCredentials
 load_dotenv()
 
 songName = input("Enter a song name. ")
+artistName = input("Enter an artist name. ")
 spotify = spotipy.Spotify(
     client_credentials_manager=SpotifyClientCredentials())
 
 # Initialize spotipy
-
-
-results = spotify.search(q="track=\""+songName+"\"", type="track", limit=1)
+results = spotify.search(
+    q="track:\""+songName+"\"+artist:\""+artistName+"\"", type="track", limit=10
+    )
 tracks = results["tracks"]
 items = tracks["items"]
-first = items[0]
-print(first["name"])
-print(first["uri"])
-print(first["popularity"])
+
+
+for item in items:
+    songUri = item["uri"]
+    track = spotify.track(track_id=songUri)
+    arrayArtistName = track["artists"][0]["name"]
+    spotifyLink = track["external_urls"]["spotify"]
+    print(arrayArtistName)
+    print(spotifyLink)
+
