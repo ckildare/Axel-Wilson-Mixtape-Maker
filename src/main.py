d34e09376c06606337ff2f2c046653a9595f2d29
Andrew Richerson <ajorigami1@gmail.com>
2022-01-22 14:53:29 -0600

    Add basic search feature

--- src/main.py
+++ src/main.py
@@ -1,1 +1,15 @@
-print("hello world")
+from dotenv import load_dotenv
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+load_dotenv()
+
+spotify = spotipy.Spotify(
+    client_credentials_manager=SpotifyClientCredentials())
+
+# Initialize spotipy
+
+
+letItBe = spotify.search(q="track=\"Let It Be\"", type="track")
+
+print(letItBe)
