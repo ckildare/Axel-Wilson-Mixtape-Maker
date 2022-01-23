a8371a82fd5af4fc97f3145dd4cbdb72702a4c4b
elijahquentin <eli.q.smith@gmail.com>
2022-01-23 01:11:32 -0600

    Update Song branch on website

--- src/server.py
+++ src/server.py
@@ -1,5 +1,5 @@
 from bottle import Bottle, run
-from Spotify import getSong, getSpotifyClient
+from Spotify import getSong, getSpotifyClient, getRecommendedSongs
 import json
 
 spotify = getSpotifyClient()
@@ -8,6 +8,12 @@ app = Bottle()
 @app.route('/selectSong')
 def selectSong():
     x = json.dumps(getSong(spotify, "Hey Jude", "Beatles").toJsonObj())
+    print(x)
+    return x
+
+@app.route('/recommendedSongs')
+def recommendedSongs():
+    x = json.dumps(getRecommendedSongs(spotify, "Hey Jude", "Beatles").toJsonObj())
     print(x)
     return x
 
