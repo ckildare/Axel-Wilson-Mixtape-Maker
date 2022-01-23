03ac7a018d2db6429be24fbfeb39c23a29fa338d
Andrew Richerson <ajorigami1@gmail.com>
2022-01-23 05:03:19 -0600

    Add functionality to search page

---
+++ frontend/src/FirstPage.js
@@ -0,0 +1,60 @@
+import { useState } from "react";
+import { useSelectSong } from "./hooks";
+export const FirstPage = () => {
+    const selectSong = useSelectSong();
+
+    const [title, setTitle] = useState("");
+    const [artist, setArtist] = useState("");
+
+    const handleSearch = async (e) => {
+        e.preventDefault()
+        const song = await selectSong(title, artist)
+        console.dir(song)
+    }
+    return (
+        <div className="row">
+            <div className="col-8 col-s-8">
+                <h1>Search the Song Here!</h1>
+                <form>
+                    <label htmlFor="trackTitle">Song Title: </label>
+                    <br />
+                    <input
+                        type="text"
+                        id="trackTitle"
+                        name="trackTitle"
+                        placeholder="Search for Song Title..."
+                        onChange={e => setTitle(e.target.value)}
+                    />
+                    <br />
+                </form>
+                <form>
+                    <label htmlFor="artist">Artist: </label>
+                    <br />
+                    <input
+                        type="text"
+                        id="artist"
+                        name="artist"
+                        placeholder="Search for Artist..."
+                        onChange={e => setArtist(e.target.value)}
+                    />
+                </form>
+                <a href="songviewer.html">
+                    <div className="button2" onClick={handleSearch}>Search</div>
+                </a>
+            </div>
+
+            <div className="col-2 col-s-2">
+                <a href="tutorial.html">
+                    <div className="button center">How To Use</div>
+                </a>
+            </div>
+
+            <div className="col-2 col-s-2">
+                <img src="img/axel.jpg" alt="Mugshot of Alex Wilson" />
+                <a href="https://open.spotify.com/">
+                    <img src="img/spotify-logo.png" alt="Spotify Logo" />
+                </a>
+            </div>
+        </div>
+    );
+};
