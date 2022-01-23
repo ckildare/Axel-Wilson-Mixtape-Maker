1010485070c63082240e6886d7a976226552d167
Connor <ckildare10@gmail.com>
2022-01-22 21:47:54 -0600

    Added img folder

--- src/gui.py
+++ src/gui.py
@@ -8,23 +8,37 @@ from PyQt5.QtWidgets import *
 class UIWindow(object):
     def setupUI(self, MainWindow):
         MainWindow.setGeometry(50, 50, 1000, 100)
-        MainWindow.setFixedSize(400, 450)
-        MainWindow.setWindowTitle("UIWindow")
+        MainWindow.setFixedSize(800, 500)
+        MainWindow.setWindowTitle("axel-wilson's-mixtape-maker")
         self.centralwidget = QWidget(MainWindow)
+
         # mainwindow.setWindowIcon(QtGui.QIcon('PhotoIcon.png'))
-        self.ToolsBTN = QPushButton('text', self.centralwidget)
-        self.ToolsBTN.move(50, 350)
+
+        self.exitButton = QPushButton('Exit', self.centralwidget)
+        self.exitButton.move(730, 480)
         MainWindow.setCentralWidget(self.centralwidget)
 
 
 class Tutorial(object):
     def setupUI(self, MainWindow):
         MainWindow.setGeometry(50, 50, 200, 450)
-        MainWindow.setFixedSize(400, 450)
-        MainWindow.setWindowTitle("Tutorial")
+        MainWindow.setFixedSize(800, 500)
+        MainWindow.setWindowTitle("axel-wilson's-mixtape-maker-tutorial")
         self.centralwidget = QWidget(MainWindow)
-        self.CPSBTN = QPushButton("text2", self.centralwidget)
-        self.CPSBTN.move(100, 350)
+        self.exitButton = QPushButton("Exit", self.centralwidget)
+        self.exitButton.move(660, 480)
+        self.returnButton = QPushButton('Exit', self.centralwidget)
+        self.returnButton.move(660, 480)
+        MainWindow.setCentralWidget(self.centralwidget)
+
+class SongView(object):
+    def setupUI(self, MainWindow):
+        MainWindow.setGeometry(50, 50, 200, 450)
+        MainWindow.setFixedSize(800, 500)
+        MainWindow.setWindowTitle("axel-wilson's-mixtape-maker")
+        self.centralwidget = QWidget(MainWindow)
+        self.exitButton = QPushButton("Exit", self.centralwidget)
+        self.exitButton.move(730, 480)
         MainWindow.setCentralWidget(self.centralwidget)
 
 
@@ -32,17 +46,22 @@ class MainWindow(QMainWindow):
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
         self.mainWindow = UIWindow()
-        self.uiToolTab = UIToolTab()
+        self.tutorial = Tutorial()
         self.startMainWindow()
 
     def startTutorial(self):
-        self.uiToolTab.setupUI(self)
-        self.uiToolTab.CPSBTN.clicked.connect(self.startMainWindow)
+        self.tutorial.setupUI(self)
+        self.tutorial.exitButton.clicked.connect(QCoreApplication.instance().quit)
+        self.show()
+
+    def startSongView(self):
+        self.songView.setupUI(self)
+        self.songView.exitButton.clicked.connect(QCoreApplication.instance().quit)
         self.show()
 
     def startMainWindow(self):
         self.mainWindow.setupUI(self)
-        self.mainWindow.ToolsBTN.clicked.connect(self.startTutorial)
+        self.mainWindow.exitButton.clicked.connect(QCoreApplication.instance().quit)
         self.show()
 
 
@@ -56,3 +75,5 @@ app = QApplication(sys.argv)
 i = Index()
 i.show()
 app.exec()
+
+# QCoreApplication.instance().quit
