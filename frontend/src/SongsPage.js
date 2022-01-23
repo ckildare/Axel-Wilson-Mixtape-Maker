03f6798351535c441007cc584b71b87686472fea
Andrew Richerson <ajorigami1@gmail.com>
2022-01-23 05:52:07 -0600

    Add song page to react and style list

--- frontend/src/SongsPage.js
+++ frontend/src/SongsPage.js
@@ -1,3 +1,36 @@
 export const SongsPage = () => {
-  return <div>Songs page</div>;
+    return (
+        <div className="row">
+            <div className="col-8 col-s-8">
+                <div className="track">
+                    <div className="track-info">
+                        <span>Artist Name</span>
+                        <span>Track Name</span>
+                        <span>Album Name</span>
+                    </div>
+                    <form>
+                        <input
+                            type="checkbox"
+                            id="like"
+                            name="like"
+                            value="like"
+                        />
+                    </form>
+                </div>
+            </div>
+
+            <div className="col-2 col-s-2">
+                <div className="button center">
+                    <a href="tutorial.html">How To Use</a>
+                </div>
+            </div>
+
+            <div className="col-2 col-s-2">
+                <img src="img/axel.jpg" alt="Mugshot of Alex Wilson" />
+                <a href="https://open.spotify.com/">
+                    <img src="img/spotify-logo.png" alt="Spotify Logo" />
+                </a>
+            </div>
+        </div>
+    );
 };
