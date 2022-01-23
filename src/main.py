239bb7a419b8b16f0e52ba284af05d7442341620
Andrew Richerson <ajorigami1@gmail.com>
2022-01-22 19:05:17 -0600

    Get 5 rec song names

--- src/main.py
+++ src/main.py
@@ -2,9 +2,14 @@ from tkinter import Tk, ttk
 from typing import List
 from dotenv import load_dotenv
 import spotipy
+from spotipy import Spotify
 from spotipy.oauth2 import SpotifyClientCredentials
 
 from Song import Song
+
+
+keptSongs: List[Song] = []
+discardedSongs: List[Song] = []
 
 
 def setUpGui():
@@ -30,7 +35,6 @@ def getSongs(spotify, songName, artistName, limit=50) -> List[Song]:
     )
     tracks = results["tracks"]
     items = tracks["items"]
-
     songs = []
     for item in items:
         songs.append(Song(item))
@@ -40,6 +44,22 @@ def getSongs(spotify, songName, artistName, limit=50) -> List[Song]:
 def getSong(spotify, songName, artistName):
     songs = getSongs(spotify, songName, artistName, 1)
     return songs[0]
+
+
+def getRecommendedSongs(spotify: Spotify, seedSongs: List[Song]) -> List[Song]:
+    seedSongIds = []
+    for song in seedSongs:
+        seedSongIds.append(song.uri)
+
+    results = spotify.recommendations(seed_tracks=seedSongIds, limit=5, )
+    tracks = results["tracks"]
+
+    recSongs = []
+    for track in tracks:
+        song = Song(track)
+        recSongs.append(song)
+
+    return recSongs
 
 
 def printSong(song: Song):
@@ -53,7 +73,14 @@ def main():
     artistName = input("Enter an artist name. ")
 
     song = getSong(spotify, songName, artistName)
+
+    print("Song:")
     printSong(song)
+
+    recSongs = getRecommendedSongs(spotify, [song])
+    print("Rec songs:")
+    for rec in recSongs:
+        printSong(rec)
 
 
 main()
