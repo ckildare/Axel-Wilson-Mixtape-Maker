bdfb22c1a9581724f5d46074d436825f1fc44eed
elijahquentin <eli.q.smith@gmail.com>
2022-01-22 23:53:52 -0600

    Updating Search algorithm.

--- src/Spotify.py
+++ src/Spotify.py
@@ -40,12 +40,12 @@ def getSong(spotify, songName, artistName):
     songs = getSongs(spotify, songName, artistName, 1)
     return songs[0]
 
-def getRecommendedSongs(spotify: Spotify, seedSongs: List[Song]) -> List[Song]:
+def getRecommendedSongs(spotify: Spotify, seedSongs: List[Song], discardedSongs: List[Song]) -> List[Song]:
     seedSongIds = []
     for song in seedSongs:
         seedSongIds.append(song.uri)
 
-    results = spotify.recommendations(seed_tracks=seedSongIds, limit=5, )
+    results = spotify.recommendations(seed_tracks=seedSongIds, limit=15, )
     tracks = results["tracks"]
 
     recSongs = []
@@ -53,7 +53,18 @@ def getRecommendedSongs(spotify: Spotify, seedSongs: List[Song]) -> List[Song]:
         song = Song(track)
         recSongs.append(song)
 
-    return recSongs
+    verifiedSongs = []
+    for recSong in recSongs:
+        verifySong = True
+        for discardSong in discardedSongs:
+            if recSong.uri == discardSong.uri:
+                verifySong = False
+        if verifySong:
+            verifiedSongs.append(recSong)
+
+    returnVerifySongs = verifiedSongs[1:5]
+
+    return returnVerifySongs
 
 def printSong(song: Song):
     print(song.name)
--- src/main.py
+++ src/main.py
@@ -11,39 +11,68 @@ import Spotify
 
 keptSongs: List[Song] = []
 discardedSongs: List[Song] = []
+seedSongs: List[Song] = []
 
 
 def main():
     spotify = Spotify.getSpotifyClient()
+    
+    keptSongs = []
+    discardedSongs = []
+    seedSongs = [] 
 
     songName = input("Enter a song name. ")
     artistName = input("Enter an artist name. ")
 
-    keptSongs = []
-    seedSongs = []
+    
     selectedSong = Spotify.getSong(spotify, songName, artistName)
     keptSongs.append(selectedSong)
+    discardedSongs.append(selectedSong)
     seedSongs.append(selectedSong)
 
     more = True
     while more:
-        recSongs = Spotify.getRecommendedSongs(spotify, seedSongs)
-        for song in recSongs:
+        verifiedSongs = Spotify.getRecommendedSongs(spotify, seedSongs, discardedSongs)
+        print("\nReccommended Songs: \n")
+        for song in verifiedSongs:
             Spotify.printSong(song)
 
-        print("For debug purposes, do you want to select the first three songs to seed more recs?")
+        print("\nFor debug purposes, do you want to pull up five more songs? (True/False) ")
         more = util.strtobool(input("More?"))
-        if more:
-            keptSongs.append(recSongs[0])
-            keptSongs.append(recSongs[1])
-            keptSongs.append(recSongs[2])
-            seedSongs.clear()
-            seedSongs.append(recSongs[0])
-            seedSongs.append(recSongs[1])
-            seedSongs.append(recSongs[2])
+        
+        
+        verifiedSongs[0].likedSong = False # First Song is Liked
+        verifiedSongs[1].likedSong = False # Second Song is not Liked
+        verifiedSongs[2].likedSong = False
+        verifiedSongs[3].likedSong = False
+        verifiedSongs[4].likedSong = False
 
+        likedSongs = False
+        for song in verifiedSongs:
+            if song.likedSong:
+                likedSongs = True
+        
+        if likedSongs:
+            seedSongs.clear()
+
+        if more: 
+            for song in verifiedSongs:
+                discardedSongs.append(song) # all songs are discarded after viewed
+                if song.likedSong:
+                    keptSongs.append(song) # send to final playlist
+                    seedSongs.append(song) # send to next recommendation
+
+
+    print("\n\n\n\n FINISHED SONGS\n")
+    print("\nKept songs: \n")
     for song in keptSongs:
         Spotify.printSong(song)
+
+    print("\nDiscardedSongs: \n")
+    for song in discardedSongs:
+        Spotify.printSong(song)
+    
+    print("\n")
 
 
 main()
