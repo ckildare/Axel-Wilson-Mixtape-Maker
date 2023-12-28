1fabe0d5b786d343d343bba29aed0012195615c2
Connor KilDARE <ckildare10@gmail.com>
2023-12-28 01:36:37 -0600

    Revert to deprecated site

--- mixtape-maker/pages/howTo.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import styles from './pageStyles/howTo.module.scss'
-
-export default function HowToPage() {
-  return (
-    <div>
-    </div>
-  )
-};
--- mixtape-maker/pages/result.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import styles from './styles/result.module.scss'
-
-export default function ResultPage() {
-  return (
-    <div>
-    </div>
-  )
-};
