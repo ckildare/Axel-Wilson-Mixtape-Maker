4c77e7de112e0ee35c887fe94f6344a88d3611c7
Connor KilDARE <ckildare10@gmail.com>
2023-12-28 00:50:04 -0600

    Added some fields for recommendation calls

--- mixtape-maker/public/testData/testMappedSong.js
+++ mixtape-maker/public/testData/testMappedSong.js
@@ -1,47 +1,37 @@
 export const testMappedSong = {
-  "id": "5roKfFHOmRVrwgQczq8XKL",
-  "name": "Lola",
+  "id": "2WRtSs0Ep9mrLV5eOAzJVq",
+  "name": "HOWDY",
   "artists": [
     {
-      "name": "Aleica",
-      "href": "https://api.spotify.com/v1/artists/0JyS4TI8NZcjQJl1Y5jscE"
-    },
-    {
-      "name": "Zunna",
-      "href": "https://api.spotify.com/v1/artists/7kezrkuYBnE989Mv0gmIZ6"
-    },
-    {
-      "name": "Mestizo Is Back",
-      "href": "https://api.spotify.com/v1/artists/5DbHAUWNEMwhEUSC4UwcKH"
-    },
-    {
-      "name": "Kreizy k",
-      "href": "https://api.spotify.com/v1/artists/3Z8i7zAQr7lPwH7pUQnlXJ"
+      "name": "T-Choppa",
+      "href": "https://api.spotify.com/v1/artists/5ZHi84IBgJ3vYKKUnJ8io0",
+      "id": "5ZHi84IBgJ3vYKKUnJ8io0"
     }
   ],
   "album": {
-    "name": "Lola",
-    "href": "https://api.spotify.com/v1/albums/6GmeJiUw8He3doBKBXkCRT",
+    "name": "HOWDY",
+    "href": "https://api.spotify.com/v1/albums/3e6a4NqKzb2i4zRis2AILc",
     "images": [
       {
         "height": 640,
-        "url": "https://i.scdn.co/image/ab67616d0000b27383d0dfcd6d112f9c62387b33",
+        "url": "https://i.scdn.co/image/ab67616d0000b273e970988d23a4895086c0ed03",
         "width": 640
       },
       {
         "height": 300,
-        "url": "https://i.scdn.co/image/ab67616d00001e0283d0dfcd6d112f9c62387b33",
+        "url": "https://i.scdn.co/image/ab67616d00001e02e970988d23a4895086c0ed03",
         "width": 300
       },
       {
         "height": 64,
-        "url": "https://i.scdn.co/image/ab67616d0000485183d0dfcd6d112f9c62387b33",
+        "url": "https://i.scdn.co/image/ab67616d00004851e970988d23a4895086c0ed03",
         "width": 64
       }
     ],
-    "releaseDate": "2023-07-06"
+    "releaseDate": "2023-10-26",
+    "id": "3e6a4NqKzb2i4zRis2AILc"
   },
-  "duration": 141176,
-  "href": "https://api.spotify.com/v1/tracks/5roKfFHOmRVrwgQczq8XKL",
-  "preview": "https://p.scdn.co/mp3-preview/dfe32ad16102c1148cf4d5679a1a347136d4ab10?cid=9fe7be0b53224126b11e0acb22cba33b"
+  "duration": 120072,
+  "href": "https://api.spotify.com/v1/tracks/2WRtSs0Ep9mrLV5eOAzJVq",
+  "preview": "https://p.scdn.co/mp3-preview/94c59eee8a95d2f7b69ed2e93c904e3282ec28a4?cid=9fe7be0b53224126b11e0acb22cba33b"
 };
