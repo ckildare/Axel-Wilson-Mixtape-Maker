7c92ded4d69f8432fd598ed1556304f050931e41
Connor KilDARE <ckildare10@gmail.com>
2023-12-29 21:43:24 -0600

    Initial how to page

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
-// import styles from './index.module.scss';
+import { useRouter } from 'next/router';
+import Image from 'next/image';
+import styles from './index.module.scss';
+import Button from 'components/Button/Button';
+import axelWilson from 'public/images/axel.png';
+import retro from 'public/images/retro.jpg';
+import Card from 'components/cards/Card/Card';
 
 const AboutPage = () => {
+  const router = useRouter();
+
   return (
-    <div>
+    <div className={styles.screenWrapper}>
+      <Card style={{ background: retro}} className={styles.axelCard}>
+        <Image priority src={axelWilson} className={styles.axel} alt={'Photo of website mascot, Axel Wilson.'} />
+        <div className={styles.axelInfo}>
+          ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl eget ultricies ultrices, nunc nunc a eros. mus dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl eget ultricies ultrices, nunc nunc a eros.
+        </div>
+      </Card>
+      <Button type={'primary'} text={'Back To Search'} onClick={() => router.push('/')} />
     </div>
   );
 };
