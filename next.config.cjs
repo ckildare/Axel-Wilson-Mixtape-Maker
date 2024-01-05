1e9a41fad79f66f6d9f648cda5d6671984fb5a01
Connor KilDARE <ckildare10@gmail.com>
2024-01-04 20:29:33 -0600

    Add env variables to next.config

--- next.config.cjs
+++ next.config.cjs
@@ -2,4 +2,8 @@ module.exports = {
   images: {
     domains: ['i.scdn.co']
   },
+  env: {
+    SPOTIFY_CLIENT_ID: process.env.SPOTIFY_CLIENT_ID,
+    SPOTIFY_CLIENT_SECRET: process.env.SPOTIFY_CLIENT_SECRET
+  },
 };
