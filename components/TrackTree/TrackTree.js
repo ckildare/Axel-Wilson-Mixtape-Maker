f599182c7b232f756536d03dcb5294d6532941cf
Connor KilDARE <ckildare10@gmail.com>
2024-01-13 20:25:10 -0600

    Better tree pathfinding

--- components/TrackTree/TrackTree.js
+++ components/TrackTree/TrackTree.js
@@ -1,9 +1,64 @@
 import TrackLeaf from 'components/TrackTree/TrackLeaf/TrackLeaf';
 import styles from './TrackTree.module.scss';
-import React from 'react';
+import React, { useMemo } from 'react';
 import Line from 'components/gradient/Line/Line';
 
 const TrackTree = ({ trackTree }) => {
+  return (
+    <div className={styles.treeWrapper}>
+      {Object.entries(trackTree).map(([rowNumber, row]) => {
+        return (<Row key={rowNumber} rowNumber={rowNumber} row={row} trackTree={trackTree} />);
+      })}
+    </div>
+  );
+};
+
+const Row = ({ rowNumber, row, trackTree }) => {
+  const outputs = row?.map((track, key) => track.isSelected ? key : undefined).filter(key => key !== undefined);
+  const inputs = () => {
+    if (rowNumber == 0) return null;
+    if (rowNumber == 1) {
+      const inputCount = Array.from(trackTree[rowNumber - 1] || []).filter(track => track.isSelected).length;
+      console.log(`inputCount for row ${rowNumber}... ${inputCount} `);
+      return {
+        1: [2],
+        2: [1, 3],
+        3: [0, 2, 4],
+        4: [0, 1, 3, 4],
+        5: [0, 1, 2, 3, 4],
+      }[inputCount] || [];
+    }
+    return Array
+      .from(trackTree[rowNumber - 1] || [])
+      .map((track, key) => track.isSelected ? key : undefined)
+      .filter(key => key != undefined);
+  };
+
+  const routes = () => {
+    if (rowNumber == 0 || !inputs() || !outputs) return [];
+    let outputsLeft = [...outputs];
+    let routesList = [];
+    let difference = 1;
+
+    while (outputsLeft.length > 0) {
+      inputs().forEach(input => {
+        outputsLeft.forEach((output, outputIX) => {
+          if (input == output) outputsLeft.splice(outputIX, 1);
+          if (Math.abs(input - output) == difference) {
+            const offset = Math.min(input, output - 1);
+            const newRoutes = Array.from(Array.from({ length: input > output ? difference + 1 : difference }, (_, index) => index + offset));
+            routesList.push(...newRoutes || []);
+            outputsLeft.splice(outputIX, 1);
+          }
+        });
+      });
+
+      difference = difference + 1;
+    }
+
+    return routesList;
+  };
+
   const bottomConnector = (track, isMiddle) =>
     track.isSelected ?
       <div style={{ width: `${isMiddle ? '4.75rem' : '100%'}` }} className={styles.bottomConnectorWrapper}>
@@ -12,43 +67,28 @@ const TrackTree = ({ trackTree }) => {
       </div>
       : <div className={styles.bottomConnectorWrapper} />;
 
-  const rightConnector = (isMiddle) =>
-    isMiddle &&
+  const rightConnector = (isMiddle, key) =>
+    (isMiddle && routes().includes(key)) &&
     <div className={styles.rightConnectorWrapper}>
       <Line className={styles.rightConnector} />
       <Line className={styles.rightConnectorBottom} />
     </div>;
 
   return (
-    <div className={styles.treeWrapper}>
-      {Object.entries(trackTree)
-        .map(([rowNumber, row]) => {
-          let inputsAndOutputIXs = [];
-          trackTree[rowNumber - 1]?.forEach((track, key) => track.isSelected && inputsAndOutputIXs.push(key));
-          row?.forEach((track, key) => track.isSelected && inputsAndOutputIXs.push(key));
-          console.log(`inputsAndOutputIXs for row ${rowNumber}... ${inputsAndOutputIXs} `);
-          const min = Math.min(...inputsAndOutputIXs);
-          const max = Math.max(...inputsAndOutputIXs);
-          console.log(`min:max for row ${rowNumber}... ${min}:${max} `);
+    <div key={rowNumber} className={styles.row}>
+      {(row || []).map((track, key) => {
+        const isMiddle = key + 1 != row.length;
 
-          return (
-            <div key={rowNumber} className={styles.row}>
-              {(row || []).map((track, key) => {
-                const isMiddle = key + 1 != row.length;
-
-                return (
-                  <div style={{ width: `${key + 1 != row.length ? '100%' : '4.75rem'}` }} key={key} className={styles.leafRow}>
-                    <div key={key} className={styles.leafColumn}>
-                      <TrackLeaf track={track} />
-                      {bottomConnector(track, isMiddle)}
-                    </div>
-                    {rightConnector(isMiddle && (key + 1 > min && key < max))}
-                  </div>
-                );
-              })}
+        return (
+          <div style={{ width: `${key + 1 != row.length ? '100%' : '4.75rem'}` }} key={key} className={styles.leafRow}>
+            <div key={key} className={styles.leafColumn}>
+              <TrackLeaf track={track} />
+              {bottomConnector(track, isMiddle)}
             </div>
-          );
-        })}
+            {rightConnector(isMiddle, key)}
+          </div>
+        );
+      })}
     </div>
   );
 };
